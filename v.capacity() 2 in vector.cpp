447f9bc70c43e9666c4a992a4aa79ea9bff443da
pravir-kadian20 <56831070+pravir-kadian20@users.noreply.github.com>
2019-11-03 11:47:14 +0530

    Add files via upload

---
+++ v.capacity() 2 in vector.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+int main() {
+
+    vector<int>v;
+    v.reserve(1000);
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int temp;
+        cin>>temp;
+        v.push_back(temp);
+    }
+    cout << v.capacity() << endl;
+    for(int i=0;i<v.size();i++){
+        cout << v[i] << " ";
+    }
+}
